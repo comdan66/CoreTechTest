f589d29420a34bb33a4b3cc8b53da72202537657
OA Wu <comdan66@gmail.com>
2018-12-03 23:33:51 +0800

    設定 Router

--- app/routers.php
+++ app/routers.php
@@ -1,6 +1,9 @@
 <?php defined('MAPLE') || exit('此檔案不允許讀取！');
 
 Router::get('')->controller('Main@index');
+Router::get('search')->controller('Main@search');
+Router::get('comment')->controller('Main@comment');
+Router::get('top')->controller('Main@top');
 
 Router::file('cli.php')   || gg('載入 Router「cli.php」失敗！');
 Router::file('admin.php') || gg('載入 Router「admin.php」失敗！');
